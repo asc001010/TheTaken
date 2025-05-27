d97831f7adc9f83cc8375538e8383b642c9dd5f1
안승찬 <115769266+asc001010@users.noreply.github.com>
2025-05-27 18:10:23 +0900

    Auto commit batch 14 (90MB)

---
+++ Assets/Horror_Prison/Other/AE_Door.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AE_Door : MonoBehaviour
+{
+
+    bool trig, open;
+    public float smooth = 2.0f;
+    public float DoorOpenAngle = 87.0f;
+    private Vector3 defaulRot;
+    private Vector3 openRot;
+    public Text txt;//text 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaulRot = transform.eulerAngles;
+        openRot = new Vector3(defaulRot.x, defaulRot.y + DoorOpenAngle, defaulRot.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
+        }
+        else
+        {
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaulRot, Time.deltaTime * smooth);
+        }
+        if (Input.GetKeyDown(KeyCode.E) && trig)
+        {
+            open = !open;
+        }
+        if (trig)
+        {
+            if (open)
+            {
+                txt.text = "Close E";
+            }
+            else
+            {
+                txt.text = "Open E";
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider coll)//вход и выход в\из  триггера 
+    {
+        if (coll.tag == "Player")
+        {
+            if (!open)
+            {
+                txt.text = "Close E ";
+            }
+            else
+            {
+                txt.text = "Open E";
+            }
+            trig = true;
+        }
+    }
+    private void OnTriggerExit(Collider coll)//вход и выход в\из  триггера 
+    {
+        if (coll.tag == "Player")
+        {
+            txt.text = " ";
+            trig = false;
+        }
+    }
+}
